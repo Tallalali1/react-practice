d24596fda994d24c5dff15129c285a649488dd40
Tallalali1 <89486859+Tallalali1@users.noreply.github.com>
2022-06-27 12:44:40 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// var React = require("react");
+import React from "react";
+import ReactDOM from "react-dom";
+// var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World </h1>
+    <p> This is paragraph </p>
+  </div>,
+  document.querySelector("#root")
+);
